5a8f9c12b3dfb13c2429ffb958604a71215173b6
Taimoormirzaa <111662986+Taimoormirzaa@users.noreply.github.com>
2023-12-15 12:19:52 +0500

    Python variables

---
+++ Python Variables.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Definition
+
+# Variables are containers for storing data.
+
+# # How To Create Variables?
+
+# Python has no command for declaring a variable.
+# A variable is created the moment you first assign a value to it.
+
+# # Example:
+
+# In[1]:
+
+
+x = 5
+
+
+# In[2]:
+
+
+name = "Taimoor"
+
+
+# # Print Function()
+
+# In[3]:
+
+
+print(x)
+
+
+# In[4]:
+
+
+print(name)
+
+
+# # Case Sensitive
+
+# In[5]:
+
+
+y = 12
+
+
+# In[6]:
+
+
+Y = 10
+
+
+# # Rules
+
+# A variable name must start with a letter or the underscore character
+
+# A variable name cannot start with a number
+
+# A variable name cannot be any of the Python keywords.
+
+# you cannot give spaces between variable names
+
+# # Example:
+
+# In[7]:
+
+
+2x = "hello"
+
+
+# In[9]:
+
+
+And = 20
+
+
+# # Multi Words Variable Names
+
+# Variable names with more than one word can be difficult to read.
+# 
+# There are several techniques you can use to make them more readable:
+# 
+# 
+
+# 1) Camel Case
+
+# 2) Pascal Case
+
+# 3) Snake Case
+
+# # Camel Case
+
+# Each word, except the first, starts with a capital letter:
+# 
+# 
+
+# In[10]:
+
+
+taimoorMirza = 1
+
+
+# # Pascal Case
+
+# Each word starts with a capital letter:
+# 
+# 
+
+# In[11]:
+
+
+TaimoorMirza = 0
+
+
+# # Snake Case
+
+# Each word is separated by an underscore character:
+
+# In[12]:
+
+
+taimoor_mirZa = 20
+
+
+# # Exercise:
+
+# Create a variable age and assign it your age.
+# 
+
+# In[13]:
+
+
+age = 22
+
+
+# Make a variable with name "color" and assign the name of your favourite color to it.
+
+# In[14]:
+
+
+color = "red"
+
+
+# Create Two variables and perform additon process.
+
+# In[15]:
+
+
+x = 20
+y = 30
+x+y
+
+
+# Create Two variables and perform substraction process.
+
+# In[16]:
+
+
+a = 15
+b = 5
+a-b
+
+
+# Create Two variables and perform multiplication process.
+
+# In[17]:
+
+
+c = 2
+h = 5
+c*h
+
+
+# Make two variables, first_name and last_name, to form a full name.
+
+# In[19]:
+
+
+first_name = "Taimoor"
+last_name = "Mirza"
+full_name = first_name+last_name
+print(full_name)
+
+
+# Declare two variables, length and width, and calculate the area of a rectangle.
+
+# In[21]:
+
+
+lenght = 20
+width = 30
+area = lenght*width
+print(area)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
